95d68b12f1cc7e1f47abf87579dd8fdaf11ab1e1
behalnihal <nihalgupta140@gmail.com>
2025-05-04 18:56:28 +0530

    Initialize project

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { FaBrain, FaGithub } from "react-icons/fa";
+
+export const Navbar = () => {
+  return (
+    <nav className="fixed w-full top-0 z-50 backdrop-blur-md bg-background/50 border-b">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div>
+            <FaBrain className=" me-[5px] w-5 h-5 inline" />
+            <span className="text-xl font-bold">Sahayak AI</span>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <a href="https://github.com/behalnihal/sahayak-ai">
+              <FaGithub className="w-5 h-5 inline" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
